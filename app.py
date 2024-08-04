258b14ce66c96ad41d2d881fcb215ffcedc33635
Omkar Kamat <69665958+Omkar270048@users.noreply.github.com>
2024-08-04 15:40:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import omr_processing
 
 
 if __name__ == "__main__":
-    image_path = r"C:\Users\trini\Downloads\final_template_omr_filled.png"
+    image_path = r"final_template_omr_filled.png"
     omr_processing.process_image(image_path)
 
     dict = omr_processing.answer
